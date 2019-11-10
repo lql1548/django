f954528085f47d51b8882fd85fd2b94c33311f06
quanjulisi <quanju@qq.com>
2019-11-10 21:25:15 +0800

    实现完整分页，首页，上一页，下一页，尾页，高亮

--- ArticleBlog/ArticleBlog/views.py
+++ ArticleBlog/ArticleBlog/views.py
@@ -14,12 +14,26 @@ def listpic(request):
 def newslistpic(request,page=1):
     page = int(page)
     # 查询数据
-    article = Article.objects.all()
+    article = Article.objects.all().order_by("id")
     # 返回数据
     paginator = Paginator(article,6)  ## 每页显示六条数据
     page_obj = paginator.page(page)
 
     # page_range = paginator.page_range[:5]
+    # 获取当前页数
+    number = page_obj.number
+    # start = number - 3
+    # end = number + 2
+    if number <=2:
+        start = 0
+        end = 5
+    elif number >=paginator.num_pages -2:
+        end = paginator.num_pages
+        start = end - 5
+    else:
+        start = number - 3
+        end = number + 2
+    page_range = paginator.page_range[start:end]
     return render(request,"newslistpic.html",locals())
 
 def base(request):
@@ -36,10 +50,14 @@ def fytest(request):
     article = Article.objects.all()
 
     paginator = Paginator(article,6)
-    # print(paginator.num_pages)
+    # print(paginator.num_pages)  ## 总页数
     # print(paginator.page_range)  ## 页码范围
 
     page_obj = paginator.page(10)
+    print(page_obj.next_page_number())
+    print(page_obj.previous_page_number())
+    # print(page_obj.has_next())
+    # print(page_obj.has_previous())
     # print(page_obj)
     # for one in page_obj:
     #     print(one)
